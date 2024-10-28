14d2153fff1af40baf14f1916e90f5ea324fd54e
RichardAgain <ric.and.mail@gmail.com>
2024-10-28 08:22:32 -0400

    paralelogram fixed

--- frontend/src/app/shared/components/landing/hero/hero.component.ts
+++ frontend/src/app/shared/components/landing/hero/hero.component.ts
@@ -4,11 +4,12 @@ import { ButtonOutlineComponent } from '../../buttons/button-outline/button-outl
 import { ThemeService } from '../../../services/theme.service';
 import { TextComponent } from "../../text/text.component";
 import { TitleComponent } from "../../title/title.component";
+import { TangramComponent } from "../../tangram/tangram.component";
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [ButtonPrimaryComponent, ButtonOutlineComponent, TextComponent, TitleComponent],
+  imports: [ButtonPrimaryComponent, ButtonOutlineComponent, TextComponent, TitleComponent, TangramComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
