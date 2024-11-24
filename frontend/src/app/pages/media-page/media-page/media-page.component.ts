a19f67f5cd07c806c6f6adc57b95bacaae2505cd
Spotito <jesuseduardo220604@gmail.com>
2024-11-24 15:32:06 -0400

    Wysiwyg
    Wysiwyg implementado

--- frontend/src/app/pages/media-page/media-page/media-page.component.ts
+++ frontend/src/app/pages/media-page/media-page/media-page.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { LayoutComponent } from "../../../shared/components/dahsboard/layout/layout.component";
-
-@Component({
-  selector: 'app-media-page',
-  standalone: true,
-  imports: [LayoutComponent],
-  templateUrl: './media-page.component.html',
-  styleUrl: './media-page.component.scss'
-})
-export class MediaPageComponent {
-
-}
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core'; 
+import { LayoutComponent } from "../../../shared/components/dahsboard/layout/layout.component"; 
+import Quill from 'quill'; 
+@Component({ selector: 'app-media-page', 
+  standalone: true, imports: [LayoutComponent], 
+  templateUrl: './media-page.component.html', 
+  styleUrls: ['./media-page.component.scss'] }) 
+  export class MediaPageComponent implements AfterViewInit { @ViewChild('editor') 
+    editorElement!: ElementRef; 
+    quillInstance: any; 
+    content: string = "";
+    ngAfterViewInit() { this.quillInstance = new Quill(this.editorElement.nativeElement, 
+      { theme: 'snow', 
+        modules: { toolbar: [ [
+          { header: [1, 2, false] }], ['bold', 'italic', 'underline'], ['link', 'blockquote', 'code-block', 'image'], [{ list: 'ordered' }, { list: 'bullet' }] ] } }); }
+          save() { this.content = this.quillInstance.root.innerHTML; console.log(this.content); }
+        }
+          
